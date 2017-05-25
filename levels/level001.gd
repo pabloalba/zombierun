a33804d203be25dd1cca2948dd908169dab1cfc2
Pablo Alba <pablo.alba@kaleidos.net>
2017-05-26 00:52:06 +0200

    Change help texts

--- levels/level001.gd
+++ levels/level001.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 var help_texts = [
-"[center]You were blissfully\ndead.\nBut somebody stole\nyour teddy bear!\nHow are you supossed\nto rest forever\nwithout it?[/center]",
-"[center]So you got back to the\nland of the living\nYou are now a zombie!\n\nSo you need something\nto stay undead...\n\n[color=#760096][b]BRAAAAAAAINS[/b][/color][/center]",
-"[center]\n\nYou can jump\nto reach brains,\nto avoid chasms,\nand to dodge spikes[/center]"
+"[center]You were placidly\ndead.\nBut somebody stole\nyour teddy bear!\nHow are you supossed\nto rest forever\nwithout it?\nYou have to find it![/center]",
+"[center]You have got back to\nthe land of the living.\nYou are now a zombie!\n\nSo you need something\nto stay healthy undead...\n\n[color=#760096][b]BRAAAAAAAINS[/b][/color][/center]",
+"[center]The button\n[img]assets/img/btn_jump_100.png[/img]\nallows you to jump.\nYou should jump to\nreach high placed brains,\nto avoid chasms,\nand to dodge spikes[/center]"
 ]
 
 func _ready():
